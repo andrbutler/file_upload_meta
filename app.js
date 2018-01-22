b8b7021cbfafa13fab4a9a9c85a6460e11a1dc81
andrbutler <andrbutler@yahoo.com>
2018-01-22 14:36:03 -0500

    fix cors error for weather

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const axios = require('axios');
 
 app.set('port', (process.env.PORT || 8080));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 app.get('/', function(req, resp){
