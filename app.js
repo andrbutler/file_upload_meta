94aa2203d8682dd93b7c091646680c6b2d76abee
andrbutler <andrbutler@yahoo.com>
2018-01-22 14:26:41 -0500

    add backend for weather app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+const axios = require('axios');
 
 app.set('port', (process.env.PORT || 8080));
 
@@ -14,6 +15,22 @@ app.get('/', function(req, resp){
 	});
 	resp.sendfile('index.html');
 });
+
+app.get('/weather', function(req, resp){
+	let lat = req.query.lat || '38.5367';
+	let lon = req.query.lon || '82.6829';
+	let key = process.env.KEY;
+	let url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&mode=json&units=imperial&APPID=' + key;
+    console.log('url: ', url);
+	axios.get(url)
+		.then(res => {
+			console.log(res.data);
+			resp.writeHead(200, {'Content-type': 'application/json'});
+			resp.end(JSON.stringify(res.data));
+		}).catch(err => {
+			console.log(err);
+		});
+	});
 
 app.post('/upload', upload.single('file'), function(req, resp){
 	resp.on('error', function(error){
